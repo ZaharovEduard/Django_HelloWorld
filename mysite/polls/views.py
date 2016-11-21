c088bd8c3884998afec615e518d9c8f8fc7d252b
ZaharovEduard <eddyzaharov@gmail.com>
2016-11-22 01:35:25 +0400

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,23 @@
 from django.http import HttpResponse
 
-def index(request):
-	return HttpResponse("<h1>Hello, world!</h1>")
+
 	
-def index_0(request):
-	response = HttpResponse()
-	response.write("<h1>Wellcome</h1>")
-	response.write("This is the polls app")
-	return response
+def index(request):
+    #install one of those REST clients in your web-browser
+	if request.method == 'GET':
+		arg_1 = request.GET['arg_1'] if 'arg_1' in request.GET else None
+		arg_2 = reguest.GET['arg_2'] if 'arg_2' in request.GET else None
+        result = {}
+        if arg_1:
+            result['arg_1'] = arg_1
+        if arg_2:
+            result['arg_2'] = arg_2
+	    return HttpRespone(json.dumps(result))
+    if request.method == 'POST':
+        #Yes, it's GET too
+        arg_1 = request.GET['arg_1'] if 'arg_1' in request.GET else None        
+        args = json.loads(request.body)
+        result = ''
+        for key, value in args.iteritems():
+            result += '{}: {}\n'.format(key, value)
+        return HttpResponse(json.loads({'expr': result}))
